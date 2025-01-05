cf500f44d807e860504dafa98f065d5c41496461
dahvde <owlsensation@gmail.com>
2025-01-04 17:50:27 -0800

    Fixed pocketbase docker configuration

--- db/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Creating superuser..."
-./pocketbase superuser upsert admin@admin.com password123
-
-echo "Starting PocketBase..."
-exec ./pocketbase serve --http=0.0.0.0:8090
