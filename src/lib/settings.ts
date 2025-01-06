0a155b7776d958b4753817c82e53630ab0e20da1
dahvde <owlsensation@gmail.com>
2025-01-05 21:24:48 -0800

    Added global vars

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -1,5 +1,13 @@
+export const maxChars = 80000;
+export const maxHash = 32;
+export const minHash = 4;
+
+// For url lengths 5-8
+export const maxShortExpire = 60 * 24 * 30;
+export const minShortExpire = 1;
+
 export default {
 	minHash: 4,
 	maxHash: 32,
-	maxChars: 20000
+	maxChars: 80000
 };
