f65132b249fcaf626a8cdd349c77d1cf440e91a1
dahvde <owlsensation@gmail.com>
2025-03-27 16:07:15 -0700

    Updated script to build executable

--- db/start.sh
+++ db/start.sh
@@ -4,5 +4,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "$SCRIPT_DIR"
 
+go build -o pocketbase ./pocketbase
+
+chmod +x pocketbase
+
 ./pocketbase superuser upsert admin@admin.com password123 &
 ./pocketbase serve --http="0.0.0.0:8090"
